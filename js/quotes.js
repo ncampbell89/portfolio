b16371ec515117125f4601bc5c7465845c89803d
Nicole Campbell <nscampbell303@gmail.com>
2017-12-17 15:05:18 -0500

    Add files via upload

--- js/quotes.js
+++ js/quotes.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
   $('#newQuote').on('click', function () {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onstatereadychange = function() {
+    var xhttps = new XMLHttpsRequest();
+    xhttps.onstatereadychange = function() {
     	for (var i = 0; i < myObj.length; i++) {
     		if (this.readyState == 4 && this.status == 200) {
     			$('#message').innerHTML = this.responseText;
     		}
     	};
     };
-    xhttp.open("GET", "Desktop/My_web_portfolio/quotes.xml", true);
-    xhttp.send();
+    xhttps.open("GET", "Desktop/My_web_portfolio/quotes.xml", true);
+    xhttps.send();
   });
 });
 
