cfc4386b3fe01aa62abc4d3f9c17bb0775cd134f
Nicole Campbell <nscampbell303@gmail.com>
2017-12-23 09:58:06 -0500

    Add files via upload

--- js/quotes.js
+++ js/quotes.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
   $('#newQuote').on('click', function () {
+
     var xhttp = new XMLHttpRequest();
-    xhttp.onstatereadychange = function() {
-    	for (var i = 0; i < myObj.length; i++) {
-    		if (this.readyState == 4 && this.status == 200) {
-    			$('#message').innerHTML = this.responseText;
-    		}
-    	};
+    var url = "quotes.txt";
+
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+    }
+};
+
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += arr[i].quote + arr[i].name;
+    }
+    document.getElementById("message").innerHTML = out;
     };
-    xhttp.open("POST", "../quotes.txt", true);
-    xhttp.send(JSON.stringify(`$#message`));
+
   });
 });
 
