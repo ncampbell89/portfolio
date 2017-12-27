6f7933a4750ac877e4cfa17a3d8379ede4b5ed5b
Nicole Campbell <nscampbell303@gmail.com>
2017-12-27 08:59:06 -0500

    Add files via upload

--- js/quotes1.js
+++ js/quotes1.js
@@ -1,12 +1,16 @@
-var quotes = [
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+	var quotes = [
 	'"Whatever the mind of man can conceive and believe, it can achieve." - Napoleon Hill',
 	'"Life is about making an impact, not making an income." - Kevin Kruse',
 	'"Strive not to be a success, but rather to be of value." - Albert Einstein',
 	'"You miss 100% of the shots you don’t take." - Wayne Gretzky',
 	'"Every strike brings me closer to the next home run." - Babe Ruth'
-]
+	]
 
-function newQuote() {
-	var randomNumber = Math.floor(Math.random() * (message.length));
-    container.innerHTML = message[randomNumber];
-}
+	function newQuote() {
+		var randomNumber = Math.floor(Math.random() * (quotes.length));
+	    document.getElementById('message').innerHTML = quotes[randomNumber];
+	}
+});
