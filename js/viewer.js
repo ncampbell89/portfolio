82f140f6903ca2e5ea7b6c8ec455d60533d7ca90
Nicole Campbell <nscampbell303@gmail.com>
2018-01-10 14:48:07 -0500

    Add files via upload

--- js/viewer.js
+++ js/viewer.js
@@ -1,44 +1,23 @@
 $(document).ready(function() {
 
-$('.keyword').hide();
-$('.x').hide();
-
-$('#search').click(function() {
-	$('.lookup').hide();
-	$('.icon').hide();
-	$('.keyword').show();
-	$('.x').show();
-});
-
-$('.x').on('click', function() {
 	$('.keyword').hide();
-	$('.lookup').show();
-	$('.icon').show();
-	$(this).hide();
-});
+	$('.x').hide();
 
-document.addEventListener('keydown', function(e) {
-	if (e.which === 13) {
-		var request = new XMLHttpRequest();
+	$('#search').click(function() {
+		$('.lookup').hide();
+		$('.icon').hide();
+		$('.keyword').show();
+		$('.keyword').val('');
+		$('.x').show();
+	});
 
-		request.open('GET', 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2');
-		request.onload = function() {
-			var data = JSON.parse(request.responseText);
-			renderHTML(data);
-		};
-
-		request.send();
-
-		function renderHTML(obj) {
-			var output = "";
-
-			for (var i = 0; i < obj.length; i++) {
-				output += "<p>" + obj[i].query + "</p>";
-			}
-
-			search.insertAdjacentHTML('beforeend', output);
-		}
-	}
-});
+	$('.x').on('click', function() {
+		$('.keyword').hide();
+		$('.lookup').show();
+		$('.icon').show();
+		$('#output').html('');
+		$('.container').css("margin-top", "120px");
+		$(this).hide();
+	});
 
 });
---
+++ js/viewer1.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	$(document).on('keydown', function(e) {
+		if (e.which === 13) {
+			// Gets search input
+			var searchTerm = $('input').val();
+
+			//API URL with search term
+			var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchTerm+"&format=json&callback=?";
+
+			$.ajax({
+				type: "GET",
+				url: url,
+				async: false,
+				dataType: "json",
+				success: function(data) {
+					//console.log(data);
+					$('#output').html('');
+					for (var i = 0; i < data[1].length; i++) { 
+						$('#output').prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p><hr></li>");
+					}
+					$('.container').css("margin-top", "0px");
+				},
+				error: function(errorMessage) {
+					alert("Error");
+				}
+			})
+		}
+	})
+});
