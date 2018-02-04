dea2fd5d7895623ad6ab4afd412c939d2f51ee9c
Nicole Campbell <nscampbell303@gmail.com>
2018-02-04 11:47:57 -0500

    Add files via upload

--- js/twitchTV.js
+++ js/twitchTV.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	// var following = [];
+	var following = [];
 	// Stream info and status api call
 	$.ajax({
 		type: 'GET',
@@ -9,9 +9,9 @@ $(document).ready(function() {
 		},
 		success: function(data1) {
 			if (data1.stream === null) {
-				$("#status").html("Free Code Camp is currently OFFLINE.");
+				$("#followerInfo .row").css("background-color", "#ff6666;");
 			} else {
-				$("#status").html("Free Code Camp is currently LIVE.");
+				$("#followerInfo .row").css("background-color", "#00cc44");
 			}
 		}
 	});
@@ -25,54 +25,50 @@ $(document).ready(function() {
 		success: function(data2) {
 			for (var i = 0; i < data2.follows.length; i++) {
 				//gets display name
-				var name = data2.follows[i].channel.display_name;
+				var displayName = data2.follows[i].channel.display_name;
 				var logo = data2.follows[i].channel.logo;
-				var status = data2.follows[i].channel.status;
+				var status;
 
 				if (logo == null) {
 					logo = "images/do_not.png";
 				}
 
+				if (status === null) {
+					status = "Offline";
+				} else {
+					status = data2.follows[i].channel.status;
+					$("followerInfo .row").addClass("online");
+				}
+
 				$("#followerInfo").prepend("<div class='row'>" + "<div class='col-md-4'>" + "<a href='https://www.twitch.tv/"+name+"'>" +
 					"<img src='"+logo+"' width='70' height='70'>" + "</a>" +
-					"</div>" + "<div class='col-md-4'>" + name + "</div>" + "<div class='col-md-4'>" + status +
+					"</div>" + "<div class='col-md-4'>" + displayName + "</div>" + "<div class='col-md-4'>" + status +
 					"</div></div>");
 			}
-			streams();
+			//streams();
 		}
 	});
 
-function streams() {
-	var followers = $("#followerInfo");
-	for(var i = 0; i < followers.length; i++) {
+var deletedFollowers = ['brunofin', 'comster404'];
+	for(var i = 0; i < deletedFollowers.length; i++) {
 		$.ajax({
 			type: 'GET',
-			url: 'https://api.twitch.tv/kraken/streams/freecodecamp',
+			url: 'https://api.twitch.tv/kraken/streams/'+deletedFollowers[i],
 			headers: {
 				'Client-ID': 'pqnxt79n3bxumafxsa83plmihlaqqe'
 			},
-			success: function(data4) {
-				$("#followerInfo .row").css({"padding-top": "10px", "padding-bottom": "10px"});
-
-				if (data4.stream === null) /*offline*/ {
-					$("#followerInfo .row").css("background-color", "#ff6666");
-				} else if (data4.stream !== null) /*online*/ {
-					$("#followerInfo .row").css("background-color", "#00cc44");
-				}
-			},
 			error: function(data3) {
 				var logo = "images/do_not.png";
-				var name = data3.statusText;
+				var displayName = data3.statusText;
 				var status = data3.status;
+				console.log(data3.statusText);
 
 				$("#followerInfo").prepend("<div class='row'>" + "<div class='col-md-4'>" + "<a href='https://www.twitch.tv/"+name+"'>" +
 					"<img src='"+logo+"' width='70' height='70'>" + "</a>" +
-					"</div>" + "<div class='col-md-4'>" + name + "</div>" + "<div class='col-md-4'>" + status +
+					"</div>" + "<div class='col-md-4'>" + displayName + "</div>" + "<div class='col-md-4'>" + status +
 					"</div></div>");
 			}
 		});
 	}
-};
-
 
 });
