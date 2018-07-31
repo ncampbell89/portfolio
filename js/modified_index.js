34dbf0662b92bf00bf552816d279c6dbdbdb1bf9
Nicole Campbell <nscampbell303@gmail.com>
2018-07-30 21:48:03 -0400

    Add files via upload

--- js/modified_index.js
+++ js/modified_index.js
@@ -1,24 +1,58 @@
-if($(window).height() < 450) {
-	$('h1').css("font-size", "5em");
-	$('#greeting').css("font-size", "1.25em");
-	$('.clockGreet').css("margin-top", "70px");
-}
-
-if($(window).height() < 400) {
-	$('h1').css("font-size", "4.5em");
-	$('.logo').css({'width':'105px', 'top':'1.5em', 'left':'3em'});
-	$('nav').css({'top':'2.5em', 'right':'2em'});
-	$('#buttons').css({'top':'4rem','right':'4rem'});
-}
-
-if($(window).height() < 350) {
-	$('h1').css("font-size", "4em");
-	$('.logo').css({'width':'95px', 'top':'1.25em', 'left':'2.5em'});
-	$('#buttons').css({'top':'3rem','right':'3rem'});
-	$('.clockGreet').css('margin-top', '30px');
-}
-
 $(document).ready(function () {
+
+	function doOnOrientationChange() {
+	    if(window.orientation == 90) {
+
+	    	if(window.screen.height <= 448) {
+				$('h1').css("font-size", "5em");
+				$('#greeting').css("font-size", "1.5em");
+				$('.clockGreet').css("margin-top", "70px");
+			}
+
+			if(window.screen.height <= 385) {
+				$('h1').css("font-size", "4.5em");
+				$('.logo').css({'width':'105px', 'top':'1.5em', 'left':'3em'});
+				$('nav').css({'top':'2.5em', 'right':'2em'});
+				$('#buttons').css({'top':'4rem','right':'4rem'});
+				$('#greeting').css("font-size", "1.25em");
+			}
+
+			if(window.screen.height <= 330) {
+	    		$('h1').css('font-size', '4em');
+	    		$('#greeting').css("font-size", "1.1em");
+	    		$('.logo').css({'width':'90px', 'left':'2.5em'});
+	    		$('#buttons').css({'top':'3rem', 'right':'3rem'});
+	    		$('.clockGreet').css('margin-top', '30px');
+	    	}
+
+	    } else if (window.orientation == 0) {
+	    	if (window.screen.width <= 448) {
+	    		$('h1').css('font-size', '5.5em');
+	    		$('#greeting').css("font-size", "1.5em");
+	    		$('.clockGreet').css("margin-top", "54px");
+	    	}
+
+	    	if (window.screen.width <= 385) {
+	    		$('h1').css('font-size', '5em');
+	    		$('#greeting').css("font-size", "1.4em");
+	    		$('.clockGreet').css("margin-top", "54px");
+	    	}
+
+	    	if(window.screen.width <= 330) {
+	    		$('h1').css('font-size', '4em');
+	    		$('#greeting').css("font-size", "1.1em");
+	    		$('.logo').css({'width':'90px', 'left':'2.5em'});
+	    		$('#buttons').css({'top':'3rem', 'right':'3rem'});
+	    	}
+	    }
+    }
+ 
+  window.addEventListener('orientationchange', doOnOrientationChange);
+ 
+  // Initial execution if needed
+  doOnOrientationChange();
+
+
 	$("#cross").hide();
 	$("#menu").hide();
 
